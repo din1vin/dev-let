5a031732eb7ec1bf80f44616be16e1bcec35ac19
dingliang <dingliang@mob.com>
2023-07-20 13:06:50 +0800

    [fea] Node List

--- algorithm/src/main/java/dl/algorithm/c05nodelist/Flatten.java
+++ algorithm/src/main/java/dl/algorithm/c05nodelist/Flatten.java
@@ -5,19 +5,19 @@ import java.util.List;
 
 class Node {
     public int val;
-    public InsertNode prev;
-    public InsertNode next;
-    public InsertNode child;
+    public Node prev;
+    public Node next;
+    public Node child;
 }
 
 public class Flatten {
-    List<InsertNode> list = new ArrayList<>();
+    List<Node> list = new ArrayList<>();
 
-    public InsertNode flatten(InsertNode head) {
+    public Node flatten(Node head) {
         dfs(head);
         for (int i = 0, j = 1; j < list.size(); i++, j++) {
-            InsertNode pre = list.get(i);
-            InsertNode cur = list.get(j);
+            Node pre = list.get(i);
+            Node cur = list.get(j);
             cur.prev = pre;
             pre.next = cur;
             pre.child = null;
@@ -25,7 +25,7 @@ public class Flatten {
         return head;
     }
 
-    private void dfs(InsertNode head) {
+    private void dfs(Node head) {
         if (head == null) return;
         list.add(head);
         dfs(head.child);
