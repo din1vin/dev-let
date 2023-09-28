e14a385056472f839f94e7235af7fee491219072
dingliang <dinl_vin@163.com>
2023-09-28 10:33:17 +0800

    数组分区点

--- algorithm/src/main/java/dl/algorithm/c07prefixsum/PivotIndex.java
+++ algorithm/src/main/java/dl/algorithm/c07prefixsum/PivotIndex.java
@@ -1,18 +1,22 @@
 package dl.algorithm.c07prefixsum;
 
+
+
 public class PivotIndex {
     public int pivotIndex(int[] nums) {
         int n = nums.length;
-        int[] sum = new int[n + 1];
-        // 初始化前缀和数组
-        for (int i = 1; i <= n; i++) {
-            sum[i] = sum[i - 1] + nums[i - 1];
-        }
+        int[] sum = new int[n];
+        sum[0] = nums[0];
         for (int i = 1; i < n; i++) {
-            if(sum[i-1] == sum[n] - sum[i+1]){
-                return i;
-            }
+            sum[i] = sum[i - 1] + nums[i];
         }
+        if(sum[n-1] - sum[0] == 0) return 0;
+        for (int i = 1; i < n - 1; i++) {
+            if(sum[i-1] == sum[n-1] - sum[i]) return i;
+        }
+        if (sum[n - 2] == 0) return n-1;
+
         return -1;
     }
+
 }
